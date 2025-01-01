7aa2a79549abcf21896d4e27885772ddf9a48007
Niko Virtala <niko.virtala@hey.com>
2025-01-01 20:10:51 +0200

    chore: enable auto update & merge

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,19 @@
 import { AwsCdkApp } from "@nikovirtala/projen-aws-cdk-app";
 const project = new AwsCdkApp({
+    autoApproveOptions: {
+        secret: "GITHUB_TOKEN",
+        allowedUsernames: ["nikovirtala"],
+    },
     cdkVersion: "2.170.0",
     defaultReleaseBranch: "main",
     deps: ["@types/aws-lambda"],
+    depsUpgradeOptions: {
+        workflowOptions: {
+            labels: ["auto-approve", "auto-merge"],
+        },
+    },
     devDeps: ["@nikovirtala/projen-aws-cdk-app"],
+    mergify: true,
     name: "application-signals-for-lambda-demo",
     projenrcTs: true,
 });
