fb411418566e09790af5d74b2a499ccd1c53820a
Niko Virtala <niko.virtala@hey.com>
2025-12-11 16:35:50 +0200

    chore: finalize project template migration

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,20 +1,11 @@
 import { AwsCdkTypeScriptAppProject } from "@nikovirtala/projen-constructs";
 
 const project = new AwsCdkTypeScriptAppProject({
-    autoApproveOptions: {
-        secret: "GITHUB_TOKEN",
-        allowedUsernames: ["nikovirtala"],
-    },
-    cdkVersion: "2.201.0",
+    cdkVersion: "2.232.1",
     defaultReleaseBranch: "main",
     deps: ["@types/aws-lambda"],
-    depsUpgradeOptions: {
-        workflowOptions: {
-            labels: ["auto-approve", "auto-merge"],
-        },
-    },
     devDeps: ["@nikovirtala/projen-constructs"],
-    homebrew: false,
     name: "application-signals-for-lambda-demo",
 });
+
 project.synth();
